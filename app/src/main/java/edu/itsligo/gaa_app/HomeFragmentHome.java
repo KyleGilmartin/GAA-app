eb547067671ebe6b8f3e14e8d2f83df89cf481e9
kyle gilmartin <46896160+kyle-gilmartin@users.noreply.github.com>
2020-11-30 20:22:40 +0000

    added a contact us form and home fragment

---
+++ app/src/main/java/edu/itsligo/gaa_app/HomeFragmentHome.java
@@ -0,0 +1,21 @@
+package edu.itsligo.gaa_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragmentHome  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.homefagment_home,container,false);
+    }
+}
--- app/src/main/java/edu/itsligo/gaa_app/MainActivity.java
+++ app/src/main/java/edu/itsligo/gaa_app/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.itsligo.gaa_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -22,11 +27,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(),Login.class));
+                startActivity(new Intent(getApplicationContext(), Login.class));
                 finish();
             }
         });
+        // bottom nav
+        BottomNavigationView botomNav = findViewById(R.id.bottom_navigation);
+        botomNav.setOnNavigationItemSelectedListener(navListerner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.homeFagment_container, new HomeFragmentHome()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListerner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragmentHome();
+                            break;
+                        case R.id.nav_news:
+                            selectedFragment = new HomeFragmentNews();
+                            break;
+                        case R.id.nav_homeContact:
+                            selectedFragment = new HomeFragmentContact();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.homeFagment_container,selectedFragment).commit();
+                    return true; // true means you want to selected a selected item
+                }
+            };
 
 
     public void DoLogin(View view) {
